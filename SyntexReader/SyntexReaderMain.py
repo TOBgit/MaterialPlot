e528aa272811a413cd635053ca56e3ab50efd110
Kaiyang <48959790+ykyyky001@users.noreply.github.com>
2022-10-06 21:49:12 +0200

    syntex reader update
    now it can calculate values. all strings(property names) are now with a value of 1. need help to read the mean value.

--- SyntexReader/SyntexReaderMain.py
+++ SyntexReader/SyntexReaderMain.py
@@ -1,12 +1,19 @@
 from lexer import Lexer
 from parser import Parser
+from Interpreter import meanInterpreter, errorPropagationInterpreter
 
 #adapted from https://github.com/davidcallanan/py-simple-math-interpreter
 
 while True:
-    text = input("calc > ")
-    lexer = Lexer(text)
-    tokens = lexer.generate_token()
-    parser = Parser(tokens)
-    tree = parser.parse()
-    print(tree)
+    try:
+        text = input("calc > ")
+        lexer = Lexer(text)
+        tokens = lexer.generate_token()
+        parser = Parser(tokens)
+        tree = parser.parse()
+        if not tree: continue
+        meanInt = meanInterpreter()
+        value = meanInt.visit(tree)
+        print(value)
+    except Exception as e:
+        print(e)
