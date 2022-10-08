372e5ff941622b19b6eb8508da1f312e782fcc88
Kaiyang <48959790+ykyyky001@users.noreply.github.com>
2022-10-08 17:55:31 +0200

    Update Interpreter.py

--- SyntaxReader/Interpreter.py
+++ SyntaxReader/Interpreter.py
@@ -11,12 +11,6 @@ class meanInterpreter:
     def visit_NumberNode(self, node):
         return Number(node.value)
 
-    def visit_PropNode(self, node):
-        return Number(1)
-        #need to read the mean value here
-
-#    def prop_mean(self, item):
-
     def visit_AddNode(self, node):
         return Number(self.visit(node.node_a).value + self.visit(node.node_b).value)
     
@@ -41,9 +35,37 @@ class meanInterpreter:
     def visit_MinusNode(self, node):
         return Number(-self.visit(node.node).value)
 
-
+##here input is mean (node.value) and sd (node.std)
 class errorPropagationInterpreter:
     def visit(self, node):
         method_name = f"visit_{type(node).__name__}"
         method = getattr(self, method_name)
         return method(node)
+
+    def visit_NumberNode(self, node):
+        return Number(node.value)
+
+#need to bring in the mean...
+    def visit_AddNode(self, node):
+        return Number((self.visit(node.node_a).value **2 + self.visit(node.node_b).value ** 2)**0.5)
+#here too...
+    def visit_SubtractNode(self, node):
+        return Number((self.visit(node.node_a).value **2 + self.visit(node.node_b).value ** 2)**0.5)
+
+    def visit_MultiplyNode(self, node):
+        return Number((self.visit(node.node_a).value **2 + self.visit(node.node_b).value ** 2)**0.5)
+
+    def visit_DivideNode(self, node):
+        try:
+            return Number((self.visit(node.node_a).value **2 + self.visit(node.node_b).value ** 2)**0.5)
+        except:
+            raise Exception("Runtime math error")
+
+    def visit_PowerNode(self, node):
+        return Number(self.visit(node.node_a).value * self.visit(node.node_b).value)
+
+    def visit_PlusNode(self, node):
+        return self.visit(node.node)
+
+    def visit_MinusNode(self, node):
+        return Number(-self.visit(node.node).value)
