7c81dd601b49a76e2ddae5a626e0d538cf682615
TienanLi <ironmantnt@live.com>
2022-10-09 22:30:51 -0700

    mod: move setAxesPopUp class to View folder

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PySide2.QtGui import QBrush, QPen, QColor, QFont
 
 from GraphicsModule import MatPlotController
 from View.AGraphicsView import AGraphicsView
+from View.AxesSelectionPopUp import setAxesPopUp
 from View.TreeView import TreeView
 from View.TableWidget import ManageItem
 
@@ -196,59 +197,6 @@ class MainWindow(QMainWindow):
     def onManageItem(self):
         self.manager = ManageItem(self.controller.model)
         self.manager.show()
-
-
-class setAxesPopUp(QDialog):
-    def __init__(self, column_candidates: List[str]):
-        super().__init__()
-        file = QFile("Axes.ui")
-        file.open(QFile.ReadOnly)
-        file.close()
-        loader = QUiLoader()
-        self.ui = loader.load(file)
-        self.propList = self.columnCandidateFilter(column_candidates)
-        self.ui.x_n.addItems(self.propList)
-        self.ui.x_d.addItems(self.propList)
-        self.ui.y_n.addItems(self.propList)
-        self.ui.y_d.addItems(self.propList)
-        self.newX = None
-        self.newY = None
-        self.ui.buttonBox.accepted.connect(self.passingInfo)
-        self.ui.buttonBox.rejected.connect(self.close)
-
-    def exec_(self):
-        return self.ui.exec_()
-
-    def passingInfo(self):
-        #here read users input and bring back the info of x and y axes
-        #n stands for numerator, d stands for denominator
-        x_n = self.ui.x_n.currentText()
-        y_n = self.ui.y_n.currentText()
-        x_d = self.ui.x_d.currentText()
-        y_d = self.ui.y_d.currentText()
-        # Default handle empty exp_box to be 1.
-        # TODO(kaiyang): newX and newY should be Latex str.
-        self.newX = [x_n, int(self.ui.x_nExp.text()) if self.ui.x_nExp.text() else 1,
-                     x_d, int(self.ui.x_nExp.text()) if self.ui.x_nExp.text() else 1]
-        self.newY = [y_n, int(self.ui.x_nExp.text()) if self.ui.x_nExp.text() else 1,
-                     y_d, int(self.ui.x_nExp.text()) if self.ui.x_nExp.text() else 1]
-
-    def returnNewXY(self):
-        # Return None if the user provides no information.
-        if self.newX:
-            return [self.newX, self.newY]
-        else:
-            return None
-
-    @staticmethod
-    def columnCandidateFilter(candidates: List[str]):
-        updatedcandidates = []
-        for candidate in candidates:
-            simcandidate = candidate.split("_")[0]
-            if simcandidate not in updatedcandidates:
-                if simcandidate.lower() != "color":
-                    updatedcandidates.append(simcandidate)
-        return updatedcandidates
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
